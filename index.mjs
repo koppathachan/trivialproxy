8b916cf956af9c7aeea9d9b4b5dd397a41026006
Akhil Sasidharan <akhilsasidharan1991@gmail.com>
2021-03-20 17:06:50 +0530

    feat: adding https support

--- index.mjs
+++ index.mjs
@@ -1,13 +1,24 @@
 #! /usr/bin/env node
 
-import * as http from 'http'
-
-const server = http.createServer()
+import * as https from 'https'
+import * as http from 'http';
+import * as fs from 'fs';
 
 const append = (obj = {}, name = '', val) => ({ ...obj, [name]: val })
 
 const processArgs = args => args.reduce((obj, arg, i) => {
     switch (arg) {
+        case '--key-path':
+            const key = fs.readFileSync(args[i+1], 'utf-8');
+            return append(obj, 'KEY', key)
+        case '--cert-path':
+            const cert = fs.readFileSync(args[i+1], 'utf-8')
+            return append(obj, 'CERT', cert)
+        case '-prot':
+            return append(obj, 'PROTOCOL', args[i+1])
+        case '-sp':
+        case '--service-port':
+            return append(obj, 'PORT', args[i+1])
         case '-h':
         case '--proxy-host':
             return append(obj, 'PROXY_HOST', args[i+1])
@@ -21,8 +32,22 @@ const processArgs = args => args.reduce((obj, arg, i) => {
 
 const {
     PROXY_HOST,
-    PROXY_PORT
+    PROXY_PORT,
+    PORT,
+    KEY,
+    CERT,
+    PROTOCOL
 } = processArgs(process.argv.slice(1))
+
+const getServer = (protocol = 'http', { key = '', cert = '' } = {}) => {
+    if (protocol === 'http') return http.createServer();
+    if (protocol === 'https') return https.createServer({
+        key, cert
+    });
+    throw new Error('Unknown service');
+}
+
+const server = getServer(PROTOCOL, { key: KEY, cert: CERT })
 
 server.on('request', (req, res) => {
     res.on('error', console.error);
@@ -34,15 +59,20 @@ server.on('request', (req, res) => {
     const opts = {
         host: PROXY_HOST || 'localhost',
         port: PROXY_PORT || '3002',
-        path: req.path,
+        path: req.path || req.url,
         method: req.method,
         headers: req.headers
     };
+    console.log(opts)
     const preq = http.request(opts, pres => {
         pres.pipe(res)
         res.writeHead(pres.statusCode, pres.headers);
-    })
+    });
     req.pipe(preq);
+    preq.on('error', (err) => {
+        res.writeHead(500)
+        res.end(err.message)
+    });
 })
 
-server.listen(4000)
+server.listen(PORT || '4000')
